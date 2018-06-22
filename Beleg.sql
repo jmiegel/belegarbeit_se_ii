da45f1c0e8a897acb668480b68664dcc20900618
tsandi <stsandi-gahun@hotmail.co.uk>
2018-06-22 11:08:28 +0200

    Add files via upload
    Änderung von vorgemerkt auf -> nutzerId wenn vorgemerkt sonst 'NULL'

--- Beleg.sql
+++ Beleg.sql
@@ -11,11 +11,9 @@ create Table bibliotheksBuch(
 	titel		VARCHAR(50) NOT NULL,
 	autor		VARCHAR(50) NOT NULL,
 	exTyp		INT,
-	vorgemerkt	INT
+	vorgemerkt	CHAR(5), --nutzerID
 )
-
---exTyp = Exemplar Typ - 0 = präsenz Exemplar, 1 = Ausleihbar
---vorgemerkt = ob das buch reserviert ist. 0 = nicht reserviert, 1 = reserviert
+--vorgemerkt = NULL wenn keiner es vorgemerkt hat sonst die nutzerID
 
 create Table ausleihe(
 	nutzerid	CHAR(5) NOT NULL,
@@ -23,12 +21,13 @@ create Table ausleihe(
 	rückgabeDatum DATE,
 	PRIMARY KEY (nutzerid, signatur),
 	FOREIGN KEY (nutzerid) REFERENCES bibliotheksNutzer(nutzerid),
-	FOREIGN KEY (signatur) REFERENCES bibliotheksBuch(signatur),
-	ON DELETE CASCADE
+	FOREIGN KEY (signatur) REFERENCES bibliotheksBuch(signatur)
+		ON DELETE CASCADE,
 	UNIQUE (signatur)
 	
 )
 
+DROP Table bibliotheksBuch
 
 --Nutzer
 INSERT INTO bibliotheksNutzer VALUES ('10001', '2019-04-01', 0)
@@ -55,26 +54,26 @@ INSERT INTO bibliotheksNutzer VALUES ('10020', '2019-05-29', 0)
 select * from bibliotheksNutzer
 
 --Bücher
-INSERT INTO bibliotheksBuch VALUES ('10000001', 'Das Geisterhaus' , 'Isabel Allende', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000002', 'Die Säulen der Erde' , 'Ken Follett', 0, 1)
-INSERT INTO bibliotheksBuch VALUES ('10000003', 'Das Parfum - Die Geschichte eines Mörders' , 'Patrick Süskind', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000004', 'Der kleine Prinz' , 'Antoine de Saint-Exupery', 0, 1)
-INSERT INTO bibliotheksBuch VALUES ('10000005', 'Der Medicus' , 'Noah Gordon', 0, 1)
-INSERT INTO bibliotheksBuch VALUES ('10000006', 'Der Alchimist' , 'Paulo Coelho', 0, 1)
-INSERT INTO bibliotheksBuch VALUES ('10000007', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000008', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000009', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000010', 'Die Päpstin' , 'Donna Woolfolk Cross', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000011', 'Tintenherz' , 'Cornelia Funke', 0, 1)
-INSERT INTO bibliotheksBuch VALUES ('10000012', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000013', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000014', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, 0)
-INSERT INTO bibliotheksBuch VALUES ('10000015', 'Der Vorleser' , 'Bernhard Schlink', 1, 0)
+INSERT INTO bibliotheksBuch VALUES ('10000001', 'Das Geisterhaus' , 'Isabel Allende', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000002', 'Die Säulen der Erde' , 'Ken Follett', 0, '10019')
+INSERT INTO bibliotheksBuch VALUES ('10000003', 'Das Parfum - Die Geschichte eines Mörders' , 'Patrick Süskind', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000004', 'Der kleine Prinz' , 'Antoine de Saint-Exupery', 0, '10017')
+INSERT INTO bibliotheksBuch VALUES ('10000005', 'Der Medicus' , 'Noah Gordon', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000006', 'Der Alchimist' , 'Paulo Coelho', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000007', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000008', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000009', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000010', 'Die Päpstin' , 'Donna Woolfolk Cross', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000011', 'Tintenherz' , 'Cornelia Funke', 0, '10015')
+INSERT INTO bibliotheksBuch VALUES ('10000012', 'Der Herr der Ringe' , 'John R. R. Tolkien', 1, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000013', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000014', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000015', 'Der Vorleser' , 'Bernhard Schlink', 1, NULL)
 
 select * from bibliotheksBuch
 
 --Ausleihe
-INSERT INTO ausleihe VALUES('10001', '10000015', '2018-5-29')
+INSERT INTO ausleihe VALUES('10001', '10000014', '2018-5-29')
 INSERT INTO ausleihe VALUES('10001', '10000005', '2018-5-29')
 INSERT INTO ausleihe VALUES('10001', '10000007', '2018-5-29')
 INSERT INTO ausleihe VALUES('10005', '10000001', '2018-6-6')
