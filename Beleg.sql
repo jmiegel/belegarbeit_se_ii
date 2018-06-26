78928ef2dc2d070529f796a1994dd006fbf0e47d
tsandi <stsandi-gahun@hotmail.co.uk>
2018-06-26 17:10:13 +0200

    Änderungen an Programm und SQL
    Es sollte alles soweit funktionieren. Die Unit-Test auch.

--- Beleg.sql
+++ Beleg.sql
@@ -1,3 +1,7 @@
+DROP TABLE ausleihe
+DROP TABLE bibliotheksNutzer
+DROP TABLE bibliotheksBuch
+
 create Table bibliotheksNutzer(
 	nutzerid	CHAR(5) NOT NULL PRIMARY KEY,
 	abDatum		DATE,
@@ -18,7 +22,7 @@ create Table bibliotheksBuch(
 create Table ausleihe(
 	nutzerid	CHAR(5) NOT NULL,
 	signatur	CHAR(8) NOT NULL,
-	rückgabeDatum DATE,
+	rÃ¼ckgabeDatum DATE,
 	PRIMARY KEY (nutzerid, signatur),
 	FOREIGN KEY (nutzerid) REFERENCES bibliotheksNutzer(nutzerid),
 	FOREIGN KEY (signatur) REFERENCES bibliotheksBuch(signatur)
@@ -26,8 +30,6 @@ create Table ausleihe(
 	UNIQUE (signatur)
 	
 )
-
-DROP Table bibliotheksBuch
 
 --Nutzer
 INSERT INTO bibliotheksNutzer VALUES ('10001', '2019-04-01', 0)
@@ -43,7 +45,7 @@ INSERT INTO bibliotheksNutzer VALUES ('10010', '2019-07-29', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10011', '2019-08-09', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10012', '2019-11-30', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10013', '2019-02-20', 0)
-INSERT INTO bibliotheksNutzer VALUES ('10014', '2018-10-29', 1)
+INSERT INTO bibliotheksNutzer VALUES ('10014', '2018-07-29', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10015', '2019-01-26', 1)
 INSERT INTO bibliotheksNutzer VALUES ('10016', '2019-08-29', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10017', '2019-06-22', 0)
@@ -51,31 +53,32 @@ INSERT INTO bibliotheksNutzer VALUES ('10018', '2019-04-30', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10019', '2019-03-29', 0)
 INSERT INTO bibliotheksNutzer VALUES ('10020', '2019-05-29', 0)
 
-select * from bibliotheksNutzer
-
---Bücher
-INSERT INTO bibliotheksBuch VALUES ('10000001', 'Das Geisterhaus' , 'Isabel Allende', 0, NULL)
-INSERT INTO bibliotheksBuch VALUES ('10000002', 'Die Säulen der Erde' , 'Ken Follett', 0, '10019')
-INSERT INTO bibliotheksBuch VALUES ('10000003', 'Das Parfum - Die Geschichte eines Mörders' , 'Patrick Süskind', 0, NULL)
+--BÃ¼cher
+INSERT INTO bibliotheksBuch VALUES ('10000001', 'Das Geisterhaus' , 'Isabel Allende', 0, '10020')
+INSERT INTO bibliotheksBuch VALUES ('10000002', 'Die SÃ¤ulen der Erde' , 'Ken Follett', 0, '10019')
+INSERT INTO bibliotheksBuch VALUES ('10000003', 'Das Parfum - Die Geschichte eines MÃ¶rders' , 'Patrick SÃ¼skind', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000004', 'Der kleine Prinz' , 'Antoine de Saint-Exupery', 0, '10017')
 INSERT INTO bibliotheksBuch VALUES ('10000005', 'Der Medicus' , 'Noah Gordon', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000006', 'Der Alchimist' , 'Paulo Coelho', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000007', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000008', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000009', 'Harry Potter und der Stein der Weisen' , 'Joanne K. Rowling', 0, NULL)
-INSERT INTO bibliotheksBuch VALUES ('10000010', 'Die Päpstin' , 'Donna Woolfolk Cross', 0, NULL)
+INSERT INTO bibliotheksBuch VALUES ('10000010', 'Die PÃ¤pstin' , 'Donna Woolfolk Cross', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000011', 'Tintenherz' , 'Cornelia Funke', 0, '10015')
 INSERT INTO bibliotheksBuch VALUES ('10000012', 'Der Herr der Ringe' , 'John R. R. Tolkien', 1, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000013', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000014', 'Der Herr der Ringe' , 'John R. R. Tolkien', 0, NULL)
 INSERT INTO bibliotheksBuch VALUES ('10000015', 'Der Vorleser' , 'Bernhard Schlink', 1, NULL)
 
-select * from bibliotheksBuch
-
 --Ausleihe
-INSERT INTO ausleihe VALUES('10001', '10000014', '2018-5-29')
-INSERT INTO ausleihe VALUES('10001', '10000005', '2018-5-29')
-INSERT INTO ausleihe VALUES('10001', '10000007', '2018-5-29')
-INSERT INTO ausleihe VALUES('10005', '10000001', '2018-6-6')
+INSERT INTO ausleihe VALUES('10001', '10000014', '2018-08-29')
+INSERT INTO ausleihe VALUES('10001', '10000005', '2018-08-29')
+INSERT INTO ausleihe VALUES('10001', '10000007', '2018-08-29')
+INSERT INTO ausleihe VALUES('10005', '10000001', '2018-06-06')
+INSERT INTO ausleihe VALUES('10014', '10000006', '2018-07-28')
+
+select * from bibliotheksBuch
+select * from bibliotheksNutzer
 
 select * from ausleihe
+SELECT ausleihe.signatur, titel, autor, exTyp, vorgemerkt, ausleihe.nutzerid, abDatum, mahnung, rÃ¼ckgabeDatum FROM ausleihe, bibliotheksBuch, bibliotheksNutzer WHERE ausleihe.nutzerid = bibliotheksNutzer.nutzerid AND ausleihe.signatur = bibliotheksBuch.signatur
